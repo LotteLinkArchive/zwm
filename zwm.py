f35b9ab8af555298566ad13e0befc6ccedd9578d
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-22 21:53:18 +0000

    Fixed a bug caused by redaction

--- zwm.py
+++ zwm.py
@@ -5,7 +5,7 @@ import lib
 
 if __name__ == '__main__':
 	g = lib.SUi()
-	c = lib.Shepherd(g, os.path.abspath(os.path.dirname(os.path.realpath(__file__))+os.sep+'assets'+os.sep+'zwm_logo.png'))
+	c = lib.Shepherd(g, os.path.abspath(os.path.dirname(os.path.realpath(__file__))+os.sep+'assets'+os.sep+'wallpaper_blank.png'))
 	m = lib.Manager(c)
 	
 	c.mainloop()
